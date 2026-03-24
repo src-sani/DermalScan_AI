4f4482ee1627490ac86de375d2d9a43add263c3c
src-sani <saneeshsanthosh0811@gmail.com>
2026-03-24 23:44:27 +0530

    add image saving functionality

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from fastapi import UploadFile, File
+import os
 
 app = FastAPI()
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
+    
+    # create uploads folder if not exists
+    os.makedirs("../uploads", exist_ok=True)
+
+    file_path = f"../uploads/{file.filename}"
+
+    # save file
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
+
     return {
-        "filename": file.filename,
-        "type": file.content_type
+        "message": "File saved successfully",
+        "filename": file.filename
     }
